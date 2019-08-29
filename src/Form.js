ade1917f88ca1b91cc45fb61cad0f52a6d6affdc
Chris Cho <chrisycho04@gmail.com>
2019-08-29 00:50:59 -0700

    refactored using hooks

--- src/Form.js
+++ src/Form.js
@@ -1,66 +1,34 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const StyledForm = styled.form`
-    border: 1px solid white;
-    padding: 2rem;
-`
+const Form = (props) => {
 
-const StyledContainer = styled.div`
-    text-align: left;
-`
-
-const StyledLabel = styled.label`
-
-`
-
-const StyledInput = styled.input`
-    display: block;
-`
-
-const StyledButton = styled.button`
-    background: white;
-    width: 100%;
-`
-
-class Form extends React.Component{
-    state = {
-        name: '',
-        email: '',
-        password: '',
-        phone: ''
+    const handleSubmit = (e) => {
+        e.preventDefault();
     }
 
-    handleInput = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-
-        this.setState({[name]: value});
-    }
-
-    render(){
-        return(
-            <StyledForm>
-                <StyledContainer>
-                    <StyledLabel>Name</StyledLabel>
-                    <StyledInput type='text' name='name' value={this.state.name} onChange={this.handleInput} placeholder='Full Name' />
-                </StyledContainer>
-                <StyledContainer>
-                    <StyledLabel>Email</StyledLabel>
-                    <StyledInput type='email' name='email' value={this.state.email} onChange={this.handleInput} placeholder='Email' />
-                </StyledContainer>
-                <StyledContainer>
-                    <StyledLabel>Password</StyledLabel>
-                    <StyledInput type='password' name='password' value={this.state.password} onChange={this.handleInput} placeholder='Password' />
-                </StyledContainer>
-                <StyledContainer>
-                    <StyledLabel>Phone</StyledLabel>
-                    <StyledInput type='tel' name='phone' value={this.state.phone} onChange={this.handleInput} placeholder='Phone' />
-                </StyledContainer>
-                <StyledButton type='submit'>Submit</StyledButton>
-            </StyledForm>
-        )
-    }
+    return(
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>First Name: </label>
+                <input type='text' name='firstName' required/>
+                <label>Last Name: </label>
+                <input type='text' name='lastName' required/>
+            </div>
+            <div>
+                <label>Email</label>
+                <input type='email' name='email' required/>
+            </div>
+            <div>
+                <label>Password</label>
+                <input type='password' name='password' required/>
+            </div>
+            <div>
+                <label>Confirm Password</label>
+                <input type='password' name='passwordConfirm' required/>
+            </div>
+            <input type='submit' value='Submit'/>
+        </form>
+    )
 }
 
 export default Form;
